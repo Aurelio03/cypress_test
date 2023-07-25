fb81a35a25baaebeec22caf609a342d1172d2e42
Aurelio <aurelio083@hotmail.com>
2023-07-24 21:11:34 -0600

    Cypress test commit

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,21 @@
+describe('DuckDuckGo Search and Verify Link', () => {
+  it('Should open DuckDuckGo, search for "The dev-friendly football API", click the first result, and verify the URL', () => {
+    // Step 1: Open URL https://duckduckgo.com
+    cy.visit('https://duckduckgo.com');
+
+    // Step 2: Enter "The dev-friendly football API" in the search box.
+    cy.get('#searchbox_input').type('The dev-friendly football API');
+
+    // Step 3: Do the Search.
+    cy.get('.searchbox_searchButton__F5Bwq').click();
+
+    // Step 4: Click on the first search result link.
+    cy.get('li:nth-child(3) > article:nth-child(1) > div:nth-child(2) > h2:nth-child(1) > a')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.visit(href);
+        // Step 5: Check if the URL contains "https://www.football-data.org/".
+        cy.request(href).its('body').should('include', 'https://www.football-data.org/');
+      });
+  });
+});
